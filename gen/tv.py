1d45887de724a2f237239e1bbcfe67f82f009356
Niko <54240096+Nik0-0@users.noreply.github.com>
2024-08-21 21:06:33 +0200

    Update tv.py

--- gen/tv.py
+++ gen/tv.py
@@ -11,7 +11,7 @@ def upscale_image(input_image_path, output_image_path, target_size):
     resized_image.save(output_image_path)
 
 
-input_image_path = "laptop.png"
+input_image_path = "/laptop/laptop.png"
 output_image_path = "/tv/tv.png"
 target_size = (1279, 1732)
 
